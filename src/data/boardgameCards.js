aebb7f9dc14faa69bf485519e5e50147c5a41d37
DarkDev <darkdev_brand@riseup.net>
2023-01-27 20:45:36 +0200

    afla v.

---
+++ src/data/boardgameCards.js
@@ -0,0 +1,1063 @@
+const cards = {
+	history: [
+		{
+			title: 'Nie było między nami wojny',
+			img: '/cards/history/nowar.svg',
+			id: 1,
+			description: ['', '', ''],
+			sources: [
+				{
+					en: [
+						{
+							title: '',
+							link: ''
+						}
+					],
+					ru: [
+						{
+							title: '',
+							link: ''
+						}
+					]
+				}
+			]
+		},
+		{
+			title: 'Zostali okupowani',
+			img: '/cards/history/occup.svg',
+			id: 2,
+			description: ['', '', ''],
+			sources: [
+				{
+					en: [
+						{
+							title: '',
+							link: ''
+						}
+					],
+					ru: [
+						{
+							title: '',
+							link: ''
+						}
+					]
+				}
+			]
+		},
+		{
+			title: 'Nienuklearni',
+			img: '/cards/history/nuclear.svg',
+			id: 3,
+			description: ['', '', ''],
+			sources: [
+				{
+					en: [
+						{
+							title: '',
+							link: ''
+						}
+					],
+					ru: [
+						{
+							title: '',
+							link: ''
+						}
+					]
+				}
+			]
+		},
+		{
+			title: 'Nie byli na Księżycu',
+			img: '/cards/history/moon.svg',
+			id: 4,
+			description: ['', '', ''],
+			sources: [
+				{
+					en: [
+						{
+							title: '',
+							link: ''
+						}
+					],
+					ru: [
+						{
+							title: '',
+							link: ''
+						}
+					]
+				}
+			]
+		},
+		{
+			title: 'Duże protesty',
+			img: '/cards/history/protest.svg',
+			id: 5,
+			description: ['', '', ''],
+			sources: [
+				{
+					en: [
+						{
+							title: '',
+							link: ''
+						}
+					],
+					ru: [
+						{
+							title: '',
+							link: ''
+						}
+					]
+				}
+			]
+		},
+		{
+			title: 'Nie byli mistrzami piłki nożnej',
+			img: '/cards/history/pilka.svg',
+			id: 6,
+			description: ['', '', ''],
+			sources: [
+				{
+					en: [
+						{
+							title: '',
+							link: ''
+						}
+					],
+					ru: [
+						{
+							title: '',
+							link: ''
+						}
+					]
+				}
+			]
+		},
+		{
+			title: 'Ogólna granica 398,624 km',
+			img: '/cards/history/border.svg',
+			id: 7,
+			description: ['', '', ''],
+			sources: [
+				{
+					en: [
+						{
+							title: '',
+							link: ''
+						}
+					],
+					ru: [
+						{
+							title: '',
+							link: ''
+						}
+					]
+				}
+			]
+		},
+		{
+			title: 'Podobne flagi',
+			img: '/cards/history/flag.svg',
+			id: 8,
+			description: ['', '', ''],
+			sources: [
+				{
+					en: [
+						{
+							title: '',
+							link: ''
+						}
+					],
+					ru: [
+						{
+							title: '',
+							link: ''
+						}
+					]
+				}
+			]
+		},
+		{
+			title: 'Pożegnali się z USSR',
+			img: '/cards/history/ussr.svg',
+			id: 9,
+			description: ['', '', ''],
+			sources: [
+				{
+					en: [
+						{
+							title: '',
+							link: ''
+						}
+					],
+					ru: [
+						{
+							title: '',
+							link: ''
+						}
+					]
+				}
+			]
+		}
+	],
+	culture: [
+		{
+			title: 'Uwielbiamy ziemniaki',
+			img: '/cards/сulture/ziemniaki.svg',
+			id: 10,
+			description: ['', '', ''],
+			sources: [
+				{
+					en: [
+						{
+							title: '',
+							link: ''
+						}
+					],
+					ru: [
+						{
+							title: '',
+							link: ''
+						}
+					]
+				}
+			]
+		},
+		{
+			title: 'Uwielbiamy pierogi',
+			img: '/cards/сulture/pierogi.svg',
+			id: 11,
+			description: ['', '', ''],
+			sources: [
+				{
+					en: [
+						{
+							title: '',
+							link: ''
+						}
+					],
+					ru: [
+						{
+							title: '',
+							link: ''
+						}
+					]
+				}
+			]
+		},
+		{
+			title: 'Uwielbiamy bigos',
+			img: '/cards/сulture/bigos.svg',
+			id: 12,
+			description: ['', '', ''],
+			sources: [
+				{
+					en: [
+						{
+							title: '',
+							link: ''
+						}
+					],
+					ru: [
+						{
+							title: '',
+							link: ''
+						}
+					]
+				}
+			]
+		},
+		{
+			title: 'Uwielbiamy barszcz',
+			img: '/cards/сulture/barszcz.svg',
+			id: 13,
+			description: ['', '', ''],
+			sources: [
+				{
+					en: [
+						{
+							title: '',
+							link: ''
+						}
+					],
+					ru: [
+						{
+							title: '',
+							link: ''
+						}
+					]
+				}
+			]
+		},
+		{
+			title: 'Podobne alfabety',
+			img: '/cards/сulture/alphabet.svg',
+			id: 14,
+			description: ['', '', ''],
+			sources: [
+				{
+					en: [
+						{
+							title: '',
+							link: ''
+						}
+					],
+					ru: [
+						{
+							title: '',
+							link: ''
+						}
+					]
+				}
+			]
+		},
+		{
+			title: 'Bohater: Wincenty Konstanty Kalinowski',
+			img: '/cards/сulture/kalinowski.svg',
+			id: 15,
+			description: ['', '', ''],
+			sources: [
+				{
+					en: [
+						{
+							title: '',
+							link: ''
+						}
+					],
+					ru: [
+						{
+							title: '',
+							link: ''
+						}
+					]
+				}
+			]
+		},
+		{
+			title: 'Bohater: Tadeusz Kościuszko',
+			img: '/cards/сulture/kościuszko.svg',
+			id: 16,
+			description: ['', '', ''],
+			sources: [
+				{
+					en: [
+						{
+							title: '',
+							link: ''
+						}
+					],
+					ru: [
+						{
+							title: '',
+							link: ''
+						}
+					]
+				}
+			]
+		},
+		{
+			title: 'Bohater: Adam Mickiewicz',
+			img: '/cards/сulture/mickiewicz.svg',
+			id: 17,
+			description: ['', '', ''],
+			sources: [
+				{
+					en: [
+						{
+							title: '',
+							link: ''
+						}
+					],
+					ru: [
+						{
+							title: '',
+							link: ''
+						}
+					]
+				}
+			]
+		}
+	],
+	territory: [
+		{
+			title: 'Wspólna rzeka: Zapadnyy Bug',
+			img: '/cards/territory/bug.svg',
+			id: 18,
+			description: ['', '', ''],
+			sources: [
+				{
+					en: [
+						{
+							title: '',
+							link: ''
+						}
+					],
+					ru: [
+						{
+							title: '',
+							link: ''
+						}
+					]
+				}
+			]
+		},
+		{
+			title: 'Wspólna rzeka: Svislach',
+			img: '/cards/territory/svislach.svg',
+			id: 19,
+			description: ['', '', ''],
+			sources: [
+				{
+					en: [
+						{
+							title: '',
+							link: ''
+						}
+					],
+					ru: [
+						{
+							title: '',
+							link: ''
+						}
+					]
+				}
+			]
+		},
+		{
+			title: 'Kraj sąsiadujący: Litwa',
+			img: '/cards/territory/litwa.svg',
+			id: 20,
+			description: ['', '', ''],
+			sources: [
+				{
+					en: [
+						{
+							title: '',
+							link: ''
+						}
+					],
+					ru: [
+						{
+							title: '',
+							link: ''
+						}
+					]
+				}
+			]
+		},
+		{
+			title: 'Kraj sąsiadujący: Łotwa',
+			img: '/cards/territory/łotwa.svg',
+			id: 21,
+			description: ['', '', ''],
+			sources: [
+				{
+					en: [
+						{
+							title: '',
+							link: ''
+						}
+					],
+					ru: [
+						{
+							title: '',
+							link: ''
+						}
+					]
+				}
+			]
+		},
+		{
+			title: 'Kraj sąsiadujący: Ukraina',
+			img: '/cards/territory/ukraina.svg',
+			id: 22,
+			description: ['', '', ''],
+			sources: [
+				{
+					en: [
+						{
+							title: '',
+							link: ''
+						}
+					],
+					ru: [
+						{
+							title: '',
+							link: ''
+						}
+					]
+				}
+			]
+		},
+		{
+			title: 'Kraj sąsiadujący: Rosja',
+			img: '/cards/territory/rus.svg',
+			id: 23,
+			description: ['', '', ''],
+			sources: [
+				{
+					en: [
+						{
+							title: '',
+							link: ''
+						}
+					],
+					ru: [
+						{
+							title: '',
+							link: ''
+						}
+					]
+				}
+			]
+		},
+		{
+			title: 'Wielkie Księstwo Litewskie',
+			img: '/cards/territory/vkl.svg',
+			id: 24,
+			description: ['', '', ''],
+			sources: [
+				{
+					en: [
+						{
+							title: '',
+							link: ''
+						}
+					],
+					ru: [
+						{
+							title: '',
+							link: ''
+						}
+					]
+				}
+			]
+		},
+		{
+			title: 'Miasta partnerskie: Viciebsk - Zielona Góra',
+			img: '/cards/territory/viciebsk_gora.svg',
+			id: 25,
+			description: ['', '', ''],
+			sources: [
+				{
+					en: [
+						{
+							title: '',
+							link: ''
+						}
+					],
+					ru: [
+						{
+							title: '',
+							link: ''
+						}
+					]
+				}
+			]
+		},
+		{
+			title: 'Miasta partnerskie: Slonim - Aleksandrów Kujawski',
+			img: '/cards/territory/slonim_kujawski.svg',
+			id: 26,
+			description: ['', '', ''],
+			sources: [
+				{
+					en: [
+						{
+							title: '',
+							link: ''
+						}
+					],
+					ru: [
+						{
+							title: '',
+							link: ''
+						}
+					]
+				}
+			]
+		},
+		{
+			title: 'Miasta partnerskie: Połack  - Gdańsk ',
+			img: '/cards/territory/połack_gdańsk.svg',
+			id: 27,
+			description: ['', '', ''],
+			sources: [
+				{
+					en: [
+						{
+							title: '',
+							link: ''
+						}
+					],
+					ru: [
+						{
+							title: '',
+							link: ''
+						}
+					]
+				}
+			]
+		},
+		{
+			title: 'Miasta partnerskie: Pinsk - Częstochowa ',
+			img: '/cards/territory/pinsk_czestochowa.svg',
+			id: 28,
+			description: ['', '', ''],
+			sources: [
+				{
+					en: [
+						{
+							title: '',
+							link: ''
+						}
+					],
+					ru: [
+						{
+							title: '',
+							link: ''
+						}
+					]
+				}
+			]
+		},
+		{
+			title: 'Miasta partnerskie: Orša - Mińsk Mazowiecki',
+			img: '/cards/territory/orša_mazowiecki.svg',
+			id: 29,
+			description: ['', '', ''],
+			sources: [
+				{
+					en: [
+						{
+							title: '',
+							link: ''
+						}
+					],
+					ru: [
+						{
+							title: '',
+							link: ''
+						}
+					]
+				}
+			]
+		},
+		{
+			title: 'Miasta partnerskie: Navapołack  - Płock ',
+			img: '/cards/territory/navapołack_płock.svg',
+			id: 30,
+			description: ['', '', ''],
+			sources: [
+				{
+					en: [
+						{
+							title: '',
+							link: ''
+						}
+					],
+					ru: [
+						{
+							title: '',
+							link: ''
+						}
+					]
+				}
+			]
+		},
+		{
+			title: 'Miasta partnerskie: Mahiloŭ - Włocławek',
+			img: '/cards/territory/mogilev_włocławek.svg',
+			id: 31,
+			description: ['', '', ''],
+			sources: [
+				{
+					en: [
+						{
+							title: '',
+							link: ''
+						}
+					],
+					ru: [
+						{
+							title: '',
+							link: ''
+						}
+					]
+				}
+			]
+		},
+		{
+			title: 'Miasta partnerskie: Minsk - Łódź',
+			img: '/cards/territory/minsk_lódź.svg',
+			id: 32,
+			description: ['', '', ''],
+			sources: [
+				{
+					en: [
+						{
+							title: '',
+							link: ''
+						}
+					],
+					ru: [
+						{
+							title: '',
+							link: ''
+						}
+					]
+				}
+			]
+		},
+		{
+			title: 'Miasta partnerskie: Mazyr - Chònice',
+			img: '/cards/territory/mazyr_сhònice.svg',
+			id: 33,
+			description: ['', '', ''],
+			sources: [
+				{
+					en: [
+						{
+							title: '',
+							link: ''
+						}
+					],
+					ru: [
+						{
+							title: '',
+							link: ''
+						}
+					]
+				}
+			]
+		},
+		{
+			title: 'Miasta partnerskie: Maladziečna  - Piotrków Trybunalski',
+			img: '/cards/territory/maladziečna_trybunalski.svg',
+			id: 34,
+			description: ['', '', ''],
+			sources: [
+				{
+					en: [
+						{
+							title: '',
+							link: ''
+						}
+					],
+					ru: [
+						{
+							title: '',
+							link: ''
+						}
+					]
+				}
+			]
+		},
+		{
+			title: 'Miasta partnerskie: Lida - Koszalin ',
+			img: '/cards/territory/lida_koszalin.svg',
+			id: 35,
+			description: ['', '', ''],
+			sources: [
+				{
+					en: [
+						{
+							title: '',
+							link: ''
+						}
+					],
+					ru: [
+						{
+							title: '',
+							link: ''
+						}
+					]
+				}
+			]
+		},
+		{
+			title: 'Miasta partnerskie: Kobryn - Bielsk Podlaski',
+			img: '/cards/territory/kobryn_bielsk.svg',
+			id: 36,
+			description: ['', '', ''],
+			sources: [
+				{
+					en: [
+						{
+							title: '',
+							link: ''
+						}
+					],
+					ru: [
+						{
+							title: '',
+							link: ''
+						}
+					]
+				}
+			]
+		},
+		{
+			title: 'Miasta partnerskie: Hrodna - Słupsk',
+			img: '/cards/territory/grodno_słupsk.svg',
+			id: 37,
+			description: ['', '', ''],
+			sources: [
+				{
+					en: [
+						{
+							title: '',
+							link: ''
+						}
+					],
+					ru: [
+						{
+							title: '',
+							link: ''
+						}
+					]
+				}
+			]
+		},
+		{
+			title: 'Miasta partnerskie: Hrodna - Białystok',
+			img: '/cards/territory/grodno_białystok.svg',
+			id: 38,
+			description: ['', '', ''],
+			sources: [
+				{
+					en: [
+						{
+							title: '',
+							link: ''
+						}
+					],
+					ru: [
+						{
+							title: '',
+							link: ''
+						}
+					]
+				}
+			]
+		},
+		{
+			title: 'Miasta partnerskie: Homiel  - Radom',
+			img: '/cards/territory/gomel_radom.svg',
+			id: 39,
+			description: ['', '', ''],
+			sources: [
+				{
+					en: [
+						{
+							title: '',
+							link: ''
+						}
+					],
+					ru: [
+						{
+							title: '',
+							link: ''
+						}
+					]
+				}
+			]
+		},
+		{
+			title: 'Miasta partnerskie: Bieraście - Biała Podlaska',
+			img: '/cards/territory/biała_brest.svg',
+			id: 40,
+			description: ['', '', ''],
+			sources: [
+				{
+					en: [
+						{
+							title: '',
+							link: ''
+						}
+					],
+					ru: [
+						{
+							title: '',
+							link: ''
+						}
+					]
+				}
+			]
+		},
+		{
+			title: 'Miasta partnerskie: Baranavičy - Biała Podlaska',
+			img: '/cards/territory/biała_baranki.svg',
+			id: 41,
+			description: ['', '', ''],
+			sources: [
+				{
+					en: [
+						{
+							title: '',
+							link: ''
+						}
+					],
+					ru: [
+						{
+							title: '',
+							link: ''
+						}
+					]
+				}
+			]
+		}
+	],
+	feasts: [
+		{
+			title: 'Święto "Święto Pracy"',
+			img: '/cards/feasts/1_maja.svg',
+			id: 42,
+			description: ['', '', ''],
+			sources: [
+				{
+					en: [
+						{
+							title: '',
+							link: ''
+						}
+					],
+					ru: [
+						{
+							title: '',
+							link: ''
+						}
+					]
+				}
+			]
+		},
+		{
+			title: 'Święto "Boże Narodzenie"',
+			img: '/cards/feasts/christmas.svg',
+			id: 43,
+			description: ['', '', ''],
+			sources: [
+				{
+					en: [
+						{
+							title: '',
+							link: ''
+						}
+					],
+					ru: [
+						{
+							title: '',
+							link: ''
+						}
+					]
+				}
+			]
+		},
+		{
+			title: 'Święto "Dożynki"',
+			img: '/cards/feasts/dożynki.svg',
+			id: 44,
+			description: ['', '', ''],
+			sources: [
+				{
+					en: [
+						{
+							title: '',
+							link: ''
+						}
+					],
+					ru: [
+						{
+							title: '',
+							link: ''
+						}
+					]
+				}
+			]
+		},
+		{
+			title: 'Święto "Dziady"',
+			img: '/cards/feasts/dziady.svg',
+			id: 45,
+			description: ['', '', ''],
+			sources: [
+				{
+					en: [
+						{
+							title: '',
+							link: ''
+						}
+					],
+					ru: [
+						{
+							title: '',
+							link: ''
+						}
+					]
+				}
+			]
+		},
+		{
+			title: 'Święto "Kolędy"',
+			img: '/cards/feasts/kolędy.svg',
+			id: 46,
+			description: ['', '', ''],
+			sources: [
+				{
+					title: '',
+					link: ''
+				}
+			]
+		},
+		{
+			title: 'Święto "Noc Kupały"',
+			img: '/cards/feasts/kupala.svg',
+			id: 47,
+			description: ['', '', ''],
+			sources: [
+				{
+					title: '',
+					link: ''
+				}
+			]
+		},
+		{
+			title: 'Święto "Walentynki"',
+			img: '/cards/feasts/walentynki.svg',
+			id: 48,
+			description: ['', '', ''],
+			sources: [
+				{
+					title: '',
+					link: ''
+				}
+			]
+		},
+		{
+			title: 'Święto "Wielkanoc"',
+			img: '/cards/feasts/wielkanoc.svg',
+			id: 49,
+			description: ['', '', ''],
+			sources: [
+				{
+					title: '',
+					link: ''
+				}
+			]
+		},
+		{
+			title: 'Święto "Dzień Kobiet"',
+			img: '/cards/feasts/womens_day.svg',
+			id: 50,
+			description: ['', '', ''],
+			sources: [
+				{
+					title: '',
+					link: ''
+				}
+			]
+		}
+	]
+};
+
+
+export default cards;
