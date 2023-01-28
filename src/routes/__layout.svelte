1bdd0b442467b2b01f2249fa98f20cd1fc888739
DarkDev <darkdev_brand@riseup.net>
2023-01-29 00:20:52 +0200

    add impact, clear styles

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -7,22 +7,28 @@
 
 
 <style>
-	:global(section) {
-		margin-bottom: 30px;
-	}
+    
+  :global(.font-impact) {
+          font-family: 'Impact';
+            line-height: 1.2;
+  }
+
+    @font-face {
+        font-family: 'Impact';
+        font-style: normal;
+        font-weight: 500;
+        
+        src: local(''),
+            /* Super Modern Browsers */ url('/fonts/impact.woff') format('woff'),
+            /* Modern Browsers */ url('/fonts/impact.ttf') format('truetype'),
+            /* Safari, Android, iOS */ url('/fonts/impact.svg') format('svg'); /* Legacy iOS */
+    }
+
 	:global(.justify-center, section)  {
         display: flex;
         justify-content: center;
     }
-    :global(.row) {
-        display: flex;
-        flex-direction: row;
-    }
-	:global(column) {
-        display: flex;
-		flex-direction: column;
-	}
-
+  
     :global(.bg) {
         background-repeat: no-repeat;
         background-position: center;
@@ -32,5 +38,25 @@
 		height: 100%;
 		margin: 0;
 		padding: 0;
+	}
+
+    @media (min-width: 1024px) {
+		:global(.bg-image-wrap) {
+			min-height: 100vh;
+		}
+
+		/* categories */
+		:global(.history) {
+			background-image: url(/images/bg/history.svg);
+		}
+		:global(.culture) {
+			background-image: url(/images/bg/culture.svg);
+		}
+		:global(.territory) {
+			background-image: url(/images/bg/territory.svg);
+		}
+		:global(.feast) {
+			background-image: url(/images/bg/feast.svg);
+		}		
 	}
 </style>
