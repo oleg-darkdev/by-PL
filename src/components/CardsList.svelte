c7830b45bcbf98448c871c12913cbb7a782dac2d
DarkDev <darkdev_brand@riseup.net>
2023-01-29 00:19:35 +0200

    upd styles

--- src/components/CardsList.svelte
+++ src/components/CardsList.svelte
@@ -29,24 +29,26 @@
 </div>
 <div class="flex w-full justify-center ">
   {#if showCards}
+  <div class="w-full counter-wrap">
     <div on:click={() => showCards = !showCards} style="width: 260px;" class="p-2 flex items-center flex-col bg-red-500 text-white rounded-lg border border-gray-200 shadow-md  hover:border-red-200 flex-col hover:bg-gray-100 hover:text-red-500 " href="/cards">
-      <h4 class="m-2 text-1xl  font-bold">
+      <h4 class="m-2 text-1xl">
         W tej kategorii
       </h4>
       <p class="flex flex-row font-normal text-3xl  leading-tight">
-        <span class="mr-6" id={id} >
+        <span class="mr-6 font-impact " id={id} >
           
         </span>
         <span >kart do gry</span>
 
       </p>
     </div>
+    </div>
     {:else}
       <div style=" " class="boardgame-image-wrap bg-white pt-6 pb-6 rounded-xl">
         <Gallery>
             {#each cardsData as boardgameCard}
                 <div class="cards-row ">
-                    <img async class='grid-item grid-item-{boardgameCard.id + 1}' src={boardgameCard.img} alt=''>
+                    <img async class='grid-item grid-item-{boardgameCard.id + 1}' src={boardgameCard.img[0]} alt=''>
                 </div>
             {/each}
         </Gallery>
