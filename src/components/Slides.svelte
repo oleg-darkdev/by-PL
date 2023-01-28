14419c9bcdbdc17d460ffe870669289cd09f435b
DarkDev <darkdev_brand@riseup.net>
2023-01-29 00:19:22 +0200

    refactoring slides component

--- src/components/Slides.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-    import cardCategories from '../data/cardCategories';
-	import TypesOfWorks from './TypesOfWorks.svelte';
-</script>
-
-{#each cardCategories as category}
-	<TypesOfWorks {category}/>
-{/each}
