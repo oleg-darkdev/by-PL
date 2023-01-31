0be99f5ef1af2caab73eca14f25834b59eae4e56
DarkDev <darkdev_brand@riseup.net>
2023-01-31 04:45:23 +0200

    add info alfa v.

--- src/data/cards/culture.js
+++ src/data/cards/culture.js
@@ -3,33 +3,44 @@ const culture = [
 		title: 'Podobne alfabety',
 		img: ['/cards/сulture/front_alphabet_by.svg', '/cards/сulture/front_alphabet_pl.svg'],
 		id: 10,
-		description: ['', '', ''],
+		description: [
+			'Łacinka białoruska, in. łacinka (biał. лацінка, łacinka) – wariant alfabetu łacińskiego, który został przystosowany do zapisu języka białoruskiego. Współczesna łacinka białoruska oparta jest o alfabet łaciński, używany w Rzeczypospolitej Obojga Narodów z dodatkiem znaków charakterystycznych dla alfabetu husyckiego oraz litery ŭ.',
+			'Po upadku Związku Radzieckiego i uzyskaniu niepodległości przez Białoruś rozpoczęto pracę nad przywróceniem łacinki do zapisu języka białoruskiego, ale używana jest ona bardzo rzadko. Posługuje się nią na przykład środowisko skupione wokół tygodnika „Nasza Niwa” i pisma Arche oraz zespół N.R.M. - większość tytułów ich płyt i piosenek jest zapisana właśnie łacinką.'
+		],
 		sourcesPl: [
 			{
-				title: 'Wikipedia: ',
-				link: ''
+				title: 'Wikipedia: Alfabet polski',
+				link: 'https://pl.wikipedia.org/wiki/Alfabet_polski'
 			},
 			{
-				title: 'Wikipedia: ',
-				link: ''
+				title: 'Adam Wolański: Współczesny polski alfabet.',
+				link: 'https://sjp.pwn.pl/poradnia/haslo/Wspolczesny-polski-alfabet;19628.html'
 			},
 			{
-				title: 'Wikipedia: ',
-				link: ''
+				title: 'Wikipedia: Alfabet białoruski (cyrylicki)',
+				link: 'https://pl.wikipedia.org/wiki/Alfabet_bia%C5%82oruski_(cyrylicki)'
+			},
+			{
+				title: 'Wikipedia: Język białoruski',
+				link: 'https://pl.wikipedia.org/wiki/J%C4%99zyk_bia%C5%82oruski'
+			},
+			{
+				title: 'Wikipedia: Łacinka białoruska',
+				link: 'https://pl.wikipedia.org/wiki/%C5%81acinka_bia%C5%82oruska'
 			}
 		],
-		sourcesRu: [
+		sourcesBe: [
 			{
-				title: 'Wikipedia: ',
-				link: ''
+				title: 'Wikipedia: Польский алфавит',
+				link: 'https://ru.wikipedia.org/wiki/%D0%9F%D0%BE%D0%BB%D1%8C%D1%81%D0%BA%D0%B8%D0%B9_%D0%B0%D0%BB%D1%84%D0%B0%D0%B2%D0%B8%D1%82'
 			},
 			{
-				title: 'Wikipedia: ',
-				link: ''
+				title: 'Wikipedia: Беларуский латинский алфавит',
+				link: 'https://ru.wikipedia.org/wiki/%D0%91%D0%B5%D0%BB%D0%BE%D1%80%D1%83%D1%81%D1%81%D0%BA%D0%B8%D0%B9_%D0%BB%D0%B0%D1%82%D0%B8%D0%BD%D1%81%D0%BA%D0%B8%D0%B9_%D0%B0%D0%BB%D1%84%D0%B0%D0%B2%D0%B8%D1%82'
 			},
 			{
-				title: 'Wikipedia: ',
-				link: ''
+				title: 'Wikipedia: Беларуский алфавит',
+				link: 'https://ru.wikipedia.org/wiki/%D0%91%D0%B5%D0%BB%D0%BE%D1%80%D1%83%D1%81%D1%81%D0%BA%D0%B8%D0%B9_%D0%B0%D0%BB%D1%84%D0%B0%D0%B2%D0%B8%D1%82'
 			}
 		]
 	},
@@ -37,33 +48,25 @@ const culture = [
 		title: 'Uwielbiamy barszcz',
 		img: ['/cards/сulture/front_barszcz_pl.svg', '/cards/сulture/front_barszcz_pl.svg'],
 		id: 11,
-		description: ['', '', ''],
+		description: [
+			'Barszcz – zupa przyrządzana na wywarze z buraków i kości, zaprawiana kwasem burakowym, popularna w krajach Europy środkowej i wschodniej. Może być w postaci czystej i zabielanej, a także podprawiany (np. śmietaną, żółtkami) lub uzupełniany innymi dodatkami[1].',
+			'Początkowo barszczem nazywano polewkę przygotowaną z kiszonych liści barszczu zwyczajnego[2]. Współcześnie barszczem nazywa się kwaśną zupę z soku z kapusty kiszonej, soku z kiszonych buraków czerwonych, kiszonego szczawiu lub innych zwykle białych kiszonych warzyw. W Polsce barszcz czerwony z buraków jest podawany na początku wieczerzy Wigilii Bożego Narodzenia. Barszcz biały podawany jest z białą, gotowaną kiełbasą, jajkiem oraz ziemniakami lub kaszą. Odmianą barszczu białego jest barszcz chrzanowy przygotowywany na wywarze z gotowanej szynki, dodatkowo zabielony śmietaną z tartym chrzanem.',
+			'Zupą podobną do barszczu białego jest żur, przygotowywany z wywaru mięsnego, zwykle wędzonego, zagęszczany zakwasem chlebowym. Tradycyjnie podawany ze smażoną kiełbasą i jajkiem oraz chlebem.'
+		],
 		sourcesPl: [
 			{
-				title: 'Wikipedia: ',
-				link: ''
+				title: 'Wikipedia: Barszcz',
+				link: 'https://pl.wikipedia.org/wiki/Barszcz_(potrawa)'
 			},
 			{
-				title: 'Wikipedia: ',
-				link: ''
-			},
-			{
-				title: 'Wikipedia: ',
-				link: ''
+				title: 'Barszcz (Polish Borscht)',
+				link: 'https://www.curiouscuisiniere.com/borscht-polish/'
 			}
 		],
-		sourcesRu: [
+		sourcesBe: [
 			{
-				title: 'Wikipedia: ',
-				link: ''
-			},
-			{
-				title: 'Wikipedia: ',
-				link: ''
-			},
-			{
-				title: 'Wikipedia: ',
-				link: ''
+				title: 'Wikipedia: Борщ',
+				link: 'https://ru.wikipedia.org/wiki/%D0%91%D0%BE%D1%80%D1%89'
 			}
 		]
 	},
@@ -71,33 +74,25 @@ const culture = [
 		title: 'Uwielbiamy bigos',
 		img: ['/cards/сulture/front_bigos_pl.svg', '/cards/сulture/front_bigos_pl.svg'],
 		id: 12,
-		description: ['', '', ''],
+		description: [
+			'Bigos – tradycyjna dla kuchni polskiej, litewskiej i białoruskiej potrawa z kapusty i mięsa. ',
+			'Pierwotnie tego słowa używano do określania sposobu siekania (zsiekać na bigos, 1534[3]), nieco później nazywano tak potrawę z siekanego mięsa (1588)[4]. W XVII w. słowo „bigos” zaczyna oznaczać także posiłek przyrządzony z resztek pozostałych z poprzednich potraw[5]. Od XVII wieku słowo było używane również w szerszym znaczeniu jako siekanina, a w znaczeniu przenośnym mówiono: „bigosować” w bójce, czyli: siekać na drobne kawałki (np. szablą).',
+			'Istnieje wiele sposobów przygotowywania bigosu i jego odmian. Wszystkie opierają się na tych samych, zasadniczych składnikach, różniąc się jedynie niektórymi dodatkami i kolejnością ich dodawania. Podstawowymi składnikami bigosu staropolskiego jest drobno szatkowana kapusta kiszona, kapusta świeża (niekiedy używa się tylko kiszonej), różne gatunki mięsa i wędlin, suszone grzyby, suszone śliwki (lub wędzone), cebula i przyprawy.'
+		],
 		sourcesPl: [
 			{
-				title: 'Wikipedia: ',
-				link: ''
+				title: 'Wikipedia: Bigos',
+				link: 'https://pl.wikipedia.org/wiki/Bigos'
 			},
 			{
-				title: 'Wikipedia: ',
-				link: ''
-			},
-			{
-				title: 'Wikipedia: ',
-				link: ''
+				title: 'Polish Food 101 ‒ Bigos',
+				link: 'https://culture.pl/en/work/polish-food-101-bigos'
 			}
 		],
-		sourcesRu: [
+		sourcesBe: [
 			{
-				title: 'Wikipedia: ',
-				link: ''
-			},
-			{
-				title: 'Wikipedia: ',
-				link: ''
-			},
-			{
-				title: 'Wikipedia: ',
-				link: ''
+				title: 'Wikipedia: Бигос',
+				link: 'https://ru.wikipedia.org/wiki/%D0%91%D0%B8%D0%B3%D0%BE%D1%81'
 			}
 		]
 	},
@@ -108,8 +103,8 @@ const culture = [
 		description: ['', '', ''],
 		sourcesPl: [
 			{
-				title: 'Wikipedia: ',
-				link: ''
+				title: 'Wikipedia: Konstanty Kalinowski',
+				link: 'https://en.wikipedia.org/wiki/Konstanty_Kalinowski'
 			},
 			{
 				title: 'Wikipedia: ',
@@ -120,18 +115,14 @@ const culture = [
 				link: ''
 			}
 		],
-		sourcesRu: [
+		sourcesBe: [
 			{
-				title: 'Wikipedia: ',
-				link: ''
+				title: 'Website about Kalinowski',
+				link: 'https://arkushy.by/kalinouski/index.htm'
 			},
 			{
-				title: 'Wikipedia: ',
-				link: ''
-			},
-			{
-				title: 'Wikipedia: ',
-				link: ''
+				title: 'Wikipedia: Калиновский, Кастусь',
+				link: 'https://ru.wikipedia.org/wiki/%D0%9A%D0%B0%D0%BB%D0%B8%D0%BD%D0%BE%D0%B2%D1%81%D0%BA%D0%B8%D0%B9,_%D0%9A%D0%B0%D1%81%D1%82%D1%83%D1%81%D1%8C'
 			}
 		]
 	},
@@ -139,33 +130,25 @@ const culture = [
 		title: 'Bohater: Tadeusz Kościuszko',
 		img: ['/cards/сulture/front_kościuszko_pl.svg', '/cards/сulture/front_kościuszko_by.svg'],
 		id: 14,
-		description: ['', '', ''],
+		description: [
+			'Tadeusz Kościuszko, właśc. Andrzej Tadeusz Bonawentura Kościuszko[1] herbu Roch III (ur. 4 lutego 1746 w Mereczowszczyźnie[1], zm. 15 października 1817 w Solurze[1]) – inżynier wojskowy[2], Najwyższy Naczelnik Siły Zbrojnej Narodowej w czasie insurekcji kościuszkowskiej, generał lejtnant wojska Rzeczypospolitej Obojga Narodów, generał major komenderujący w Dywizji Wielkopolskiej w 1792 roku[3], brevet generał brygady Armii Kontynentalnej w czasie wojny o niepodległość Stanów Zjednoczonych. ',
+			'Tadeusz Kościuszko jest uznawany za bohatera narodowego Polski, Stanów Zjednoczonych Ameryki oraz Białorusi. ',
+			'14 października 1861, w przeddzień patriotycznych obchodów rocznicy śmierci Tadeusza Kościuszki, rosyjski namiestnik Karol Lambert wprowadził w Królestwie Polskim stan wojenny. Brutalne stłumienie uroczystości rocznicowych przez wojsko rosyjskie, połączone m.in. ze sprofanowaniem kościołów, przyspieszyło przygotowania powstańcze. Po II wojnie światowej jego imieniem nazwano hutę w Chorzowie. W wielu polskich miastach znajdują się ulice im. Tadeusza Kościuszki, które spotyka się również za granicą – głównie w USA, na Białorusi, na Ukrainie oraz na Węgrzech (Budapeszt), w Serbii (Belgrad), Rosji (Petersburg, Wołgograd i Kopiejsk) i Kazachstanie (Karaganda). Istnieje też wiele parków jego imienia, np. Kosciuszko Park w Chicago oraz w mieście Milwaukee w stanie Wisconsin.'
+		],
 		sourcesPl: [
 			{
-				title: 'Wikipedia: ',
-				link: ''
+				title: 'Wikipedia: Tadeusz Kościuszko',
+				link: 'https://pl.wikipedia.org/wiki/Tadeusz_Ko%C5%9Bciuszko'
 			},
 			{
-				title: 'Wikipedia: ',
-				link: ''
-			},
-			{
-				title: 'Wikipedia: ',
-				link: ''
+				title: 'Artykuł o Kościuszce na stronach muzeum Jeffersona w Monticello',
+				link: 'https://www.monticello.org/research-education/thomas-jefferson-encyclopedia/jefferson-engraving-sokolnicki-engraving/'
 			}
 		],
-		sourcesRu: [
+		sourcesBe: [
 			{
-				title: 'Wikipedia: ',
-				link: ''
-			},
-			{
-				title: 'Wikipedia: ',
-				link: ''
-			},
-			{
-				title: 'Wikipedia: ',
-				link: ''
+				title: 'Wikipedia: Костюшко, Тадеуш',
+				link: 'https://ru.wikipedia.org/wiki/%D0%9A%D0%BE%D1%81%D1%82%D1%8E%D1%88%D0%BA%D0%BE,_%D0%A2%D0%B0%D0%B4%D0%B5%D1%83%D1%88'
 			}
 		]
 	},
@@ -173,33 +156,24 @@ const culture = [
 		title: 'Bohater: Adam Mickiewicz',
 		img: ['/cards/сulture/front_mickiewicz_pl.svg', '/cards/сulture/front_mickiewicz_pl.svg'],
 		id: 15,
-		description: ['', '', ''],
+		description: [
+			'Adam Bernard Mickiewicz (ur. 24 grudnia 1798 w Zaosiu lub Nowogródku[a][1][2], zm. 26 listopada 1855 w Stambule[3]) – polski poeta, działacz polityczny, publicysta, tłumacz, filozof, działacz religijny, mistyk, organizator i dowódca wojskowy, nauczyciel akademicki.',
+			'Znany przede wszystkim jako autor ballad, powieści poetyckich, dramatu Dziady oraz epopei narodowej Pan Tadeusz uznawanej za ostatni wielki epos kultury szlacheckiej w Rzeczypospolitej Obojga Narodów. Narodowy poeta Polski, Litwy i Białorusi. Działacz niepodległościowy, organizator polskich oddziałów do walki z Rosją, bonapartysta.'
+		],
 		sourcesPl: [
 			{
-				title: 'Wikipedia: ',
-				link: ''
-			},
-			{
-				title: 'Wikipedia: ',
-				link: ''
-			},
-			{
-				title: 'Wikipedia: ',
-				link: ''
+				title: 'Wikipedia: Adam Mickiewicz',
+				link: 'https://pl.wikipedia.org/wiki/Adam_Mickiewicz'
 			}
 		],
-		sourcesRu: [
+		sourcesBe: [
 			{
-				title: 'Wikipedia: ',
-				link: ''
+				title: 'Культура – Таямніца Міцкевіча “па кудзелі”',
+				link: 'http://kimpress.by/index.phtml?page=2&id=12603'
 			},
 			{
-				title: 'Wikipedia: ',
-				link: ''
-			},
-			{
-				title: 'Wikipedia: ',
-				link: ''
+				title: 'Wikipedia: Мицкевич, Адам',
+				link: 'https://ru.wikipedia.org/wiki/%D0%9C%D0%B8%D1%86%D0%BA%D0%B5%D0%B2%D0%B8%D1%87,_%D0%90%D0%B4%D0%B0%D0%BC'
 			}
 		]
 	},
@@ -207,33 +181,25 @@ const culture = [
 		title: 'Uwielbiamy pierogi',
 		img: ['/cards/сulture/front_pierogi_pl.svg', '/cards/сulture/front_pierogi_pl.svg'],
 		id: 16,
-		description: ['', '', ''],
+		description: [
+			'Pierogi – ogólnie: kawałki cienkiego, elastycznego i dobrze zlepiającego się ciasta napełnione najrozmaitszymi farszami i ugotowane w wodzie lub na parze, upieczone, usmażone czy grillowane; ciasto może zostać zrobione z dowolnego rodzaju mąki (pszennej, ryżowej, kukurydzianej, żytniej, gryczanej) z dodatkiem wody, jaj, drożdży i tłuszczu (masła, oleju, smalcu), śmietany oraz innych dodatków smakowo-zapachowo-barwiących[1]; przy czym ciasto powinno być dostosowane do farszu[2]; pierogi mogą mieć rozmaite kształty np. stożka, sakiewki, trójkąta, półkrążka; potrawa, którą można spotkać w wielu kuchniach na całym świecie, np. w kuchni polskiej, włoskiej, rosyjskiej, ukraińskiej, meksykańskiej, indyjskiej czy żydowskiej; jest to kulinarny koncept pochodzący z Chin, który na ziemie polskie dotarł prawdopodobnie w XIII wieku.',
+			'W Polsce: potrawa mączna kuchni staropolskiej i polskiej, słone lub słodkie nadzienie zawinięte w kawałek najczęściej pszennego ciasta makaronowego, rzadziej drożdżowego. Ze względu na proces obróbki termicznej wyróżnia się trzy rodzaje pierogów: gotowane (najbardziej powszechne), pieczone i smażone. Pierogi mogą być różnej wielkości: od maleńkich uszek aż po olbrzymi pojedynczy pieróg, który kroi się w plastry przed podaniem.',
+			'Według legendy Jacek Odrowąż zachwycił się ich smakiem podczas bytności w Kijowie[21] i dzięki niemu zagościły w Polsce[b]. Miał on karmić pierogami polską biedotę[23][24]. Współcześnie pierogi są uważane przez Polaków, jak i cudzoziemców, za potrawę typowo polską, narodową, jednak przyjmuje się, że pomysł nadziewania kawałków ciasta makaronowego różnymi rodzajami farszu i ich gotowania, pieczenia lub smażenia pochodzi z Chin[c], skąd przez Ruś dotarł do Polski, a następnie rozpowszechnił się dalej na Zachód aż do Francji.'
+		],
 		sourcesPl: [
 			{
-				title: 'Wikipedia: ',
-				link: ''
+				title: 'Wikipedia: Pierogi',
+				link: 'https://pl.wikipedia.org/wiki/Pierogi'
 			},
 			{
-				title: 'Wikipedia: ',
-				link: ''
-			},
-			{
-				title: 'Wikipedia: ',
-				link: ''
+				title: 'Polish Food 101 ‒ Pierogi',
+				link: 'https://culture.pl/en/work/polish-food-101-pierogi'
 			}
 		],
-		sourcesRu: [
+		sourcesBe: [
 			{
-				title: 'Wikipedia: ',
-				link: ''
-			},
-			{
-				title: 'Wikipedia: ',
-				link: ''
-			},
-			{
-				title: 'Wikipedia: ',
-				link: ''
+				title: 'Wikipedia: Пельмени',
+				link: 'https://ru.wikipedia.org/wiki/%D0%9F%D0%B5%D0%BB%D1%8C%D0%BC%D0%B5%D0%BD%D0%B8'
 			}
 		]
 	},
@@ -241,33 +207,29 @@ const culture = [
 		title: 'Uwielbiamy ziemniaki',
 		img: ['/cards/сulture/front_ziemniaki_pl.svg', '/cards/сulture/front_ziemniaki_pl.svg'],
 		id: 17,
-		description: ['', '', ''],
+		description: [
+			'W niedokończonym i niewydanym za życia poemacie "Kartofla" Adam Mickiewicz opisuje uroki życia na łonie natury, gdy sielankę przerywa wydobywający się z pieca... kartofel, domagając się poświęcenia mu uwagi. W 1819 roku, gdy utwór powstawał, ziemniaki były już upowszechnione na ziemiach polskich.',
+			'Historia ziemniaka w Polsce liczy ponad 300 lat, ale nie od razu był on sobą. Chyba żadne warzywo nie doczekało się tylu nazw literackich, gwarowych czy regionalnych – dość wspomnieć pyrki, pyry, grule, a nawet rzepę. "Ziemniak" utrwalił się dopiero w XX wieku. W wielu XIX-wiecznych książkach występuje termin "kartofel".',
+			''
+		],
 		sourcesPl: [
 			{
-				title: 'Wikipedia: ',
-				link: ''
+				title: 'Polska historia ziemniaka',
+				link: 'https://culture.pl/pl/artykul/polska-historia-ziemniaka'
 			},
 			{
-				title: 'Wikipedia: ',
-				link: ''
-			},
-			{
-				title: 'Wikipedia: ',
-				link: ''
+				title: 'Wikipedia: Ziemniaki',
+				link: 'https://pl.wikipedia.org/wiki/Ziemniak'
 			}
 		],
-		sourcesRu: [
+		sourcesBe: [
 			{
-				title: 'Wikipedia: ',
-				link: ''
+				title: 'Wikipedia: Бульба',
+				link: 'https://be.wikipedia.org/wiki/%D0%91%D1%83%D0%BB%D1%8C%D0%B1%D0%B0'
 			},
 			{
-				title: 'Wikipedia: ',
-				link: ''
-			},
-			{
-				title: 'Wikipedia: ',
-				link: ''
+				title: 'TOP-10 блюд белорусской национальной кухни',
+				link: 'https://kulinar.brsmok.by/kulinar/top-10-blyud-belorusskoy-natsionalnoy-kuhn.html'
 			}
 		]
 	}
