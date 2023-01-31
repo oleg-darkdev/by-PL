0e6b50422e45322e8772796fa68155944a433633
DarkDev <darkdev_brand@riseup.net>
2023-01-31 23:09:02 +0200

    alfa v.

--- src/data/cards/history.js
+++ src/data/cards/history.js
@@ -19,16 +19,12 @@ const history = [
 			{
 				title: 'Wikipedia: Historia Polski',
 				link: 'https://pl.wikipedia.org/wiki/Historia_Polski'
-			},
-			{
-				title: 'Wikipedia: ',
-				link: ''
 			}
 		],
 		sourcesBe: [
 			{
-				title: 'Wikipedia: ',
-				link: ''
+				title: 'Wikipedia: Гісторыя Беларусі',
+				link: 'https://be-tarask.wikipedia.org/wiki/%D0%93%D1%96%D1%81%D1%82%D0%BE%D1%80%D1%8B%D1%8F_%D0%91%D0%B5%D0%BB%D0%B0%D1%80%D1%83%D1%81%D1%96'
 			}
 		]
 	},
@@ -36,33 +32,33 @@ const history = [
 		title: 'Zostali okupowani',
 		img: ['/cards/history/front_occup_pl.svg', '/cards/history/front_occup_by.svg'],
 		id: 2,
-		description: ['', '', ''],
+		description: [
+			'Po przegranej Wojnie Obronnej 1939 roku Polska znalazła się pod okupacją hitlerowskich Niemiec i komunistycznego Związku Radzieckiego. Przez blisko sześć lat Polacy toczyli walkę o wolność i suwerenność, mierząc się z trudami brutalnego reżimu najeźdźców. Okupacja była dramatem milionów ludzi prześladowanych przez Niemców i Sowietów.',
+			'Ze względu na specyfikę wprowadzonego na okupowanych ziemiach reżimu i terroru Polska odróżniała się od innych krajów Europy zajętych przez wrogie armie. Okupacyjny reżim był tu niezwykle ciężki, czego dowodem jest druzgocący bilans ofiar i strat materialnych państwa polskiego.',
+			'Walki w trakcie "Operacji Barbarossa" przyniosły Białorusi wiele zniszczeń. Wycofująca się Armia Czerwona stosowała taktykę spalonej ziemi, a Niemcy nie przebierali w środkach podczas natarcia.'
+		],
 		sourcesPl: [
 			{
-				title: 'Wikipedia: ',
-				link: ''
+				title: 'Okupacja Polski w czasie II wojny światowej',
+				link: 'https://warhist.pl/okupacja/'
 			},
 			{
-				title: 'Wikipedia: ',
-				link: ''
+				title: 'Historia Białorusi. Część 16: Okupacja niemiecka',
+				link: 'https://www.polskieradio.pl/39/156/Artykul/2585006,Historia-Bialorusi-Czesc-16-Okupacja-niemiecka'
 			},
 			{
-				title: 'Wikipedia: ',
-				link: ''
+				title: 'Wikipedia: Okupacja Polski w czasie II wojny światowej',
+				link: 'https://pl.wikipedia.org/wiki/Okupacja_niemiecka_ziem_polskich_(1939%E2%80%931945)'
 			}
 		],
 		sourcesBe: [
 			{
-				title: 'Wikipedia: ',
-				link: ''
+				title: 'Wikipedia: Немецкая оккупация Белоруссии (1941—1944)',
+				link: 'https://ru.wikipedia.org/wiki/%D0%9D%D0%B5%D0%BC%D0%B5%D1%86%D0%BA%D0%B0%D1%8F_%D0%BE%D0%BA%D0%BA%D1%83%D0%BF%D0%B0%D1%86%D0%B8%D1%8F_%D0%91%D0%B5%D0%BB%D0%BE%D1%80%D1%83%D1%81%D1%81%D0%B8%D0%B8_(1941%E2%80%941944)'
 			},
 			{
-				title: 'Wikipedia: ',
-				link: ''
-			},
-			{
-				title: 'Wikipedia: ',
-				link: ''
+				title: 'Wikipedia: Немецкая оккупация Польши (1939—1945)',
+				link: 'https://ru.wikipedia.org/wiki/%D0%9D%D0%B5%D0%BC%D0%B5%D1%86%D0%BA%D0%B0%D1%8F_%D0%BE%D0%BA%D0%BA%D1%83%D0%BF%D0%B0%D1%86%D0%B8%D1%8F_%D0%9F%D0%BE%D0%BB%D1%8C%D1%88%D0%B8_(1939%E2%80%941945)'
 			}
 		]
 	},
@@ -70,33 +66,32 @@ const history = [
 		title: 'Nienuklearni',
 		img: ['/cards/history/front_nuclear_pl.svg', '/cards/history/front_nuclear_pl.svg'],
 		id: 3,
-		description: ['', '', ''],
+		description: [
+			'Polska podczas zimnej wojny posiadała aktywny program rozwoju broni masowego rażenia, a na potrzeby Ludowego Wojska Polskiego zmagazynowano od 178 do 250 sowieckich ładunków nuklearnych. Od czasu, kiedy podpisano układ o nierozprzestrzenianiu broni jądrowej (NPT), ograniczono rozrost badań nuklearnych do niewojskowych celów. Polska i Rosja pracują nad wyeliminowaniem zapasów broni chemicznej i biologicznej stworzonej przez państwa Układu Warszawskiego. ',
+			'Polska ratyfikowała Protokół genewski 4 lutego 1929 roku. Produkcję broni chemicznej w Polsce rozpoczęto przed II wojną światową. '
+		],
 		sourcesPl: [
 			{
-				title: 'Wikipedia: ',
-				link: ''
+				title: 'Wikipedia: Broń masowego rażenia w Polsce',
+				link: 'https://pl.wikipedia.org/wiki/Bro%C5%84_masowego_ra%C5%BCenia_w_Polsce'
 			},
 			{
-				title: 'Wikipedia: ',
-				link: ''
+				title: 'Wikipedia: Państwa posiadające broń jądrową',
+				link: 'https://pl.wikipedia.org/wiki/Pa%C5%84stwa_posiadaj%C4%85ce_bro%C5%84_j%C4%85drow%C4%85'
 			},
 			{
-				title: 'Wikipedia: ',
-				link: ''
+				title: 'Odkryto nieznane elementy radzieckich baz atomowych w Polsce.',
+				link: 'https://naukawpolsce.pl/aktualnosci/news%2C31296%2Codkryto-nieznane-elementy-radzieckich-baz-atomowych-w-polsce.html'
 			}
 		],
 		sourcesBe: [
 			{
-				title: 'Wikipedia: ',
-				link: ''
+				title: 'Wikipedia: Ядерное разоружение Белоруссии',
+				link: 'https://ru.wikipedia.org/wiki/%D0%AF%D0%B4%D0%B5%D1%80%D0%BD%D0%BE%D0%B5_%D1%80%D0%B0%D0%B7%D0%BE%D1%80%D1%83%D0%B6%D0%B5%D0%BD%D0%B8%D0%B5_%D0%91%D0%B5%D0%BB%D0%BE%D1%80%D1%83%D1%81%D1%81%D0%B8%D0%B8'
 			},
 			{
-				title: 'Wikipedia: ',
-				link: ''
-			},
-			{
-				title: 'Wikipedia: ',
-				link: ''
+				title: 'Wikipedia: Вооружённые силы Польши',
+				link: 'https://ru.wikipedia.org/wiki/%D0%92%D0%BE%D0%BE%D1%80%D1%83%D0%B6%D1%91%D0%BD%D0%BD%D1%8B%D0%B5_%D1%81%D0%B8%D0%BB%D1%8B_%D0%9F%D0%BE%D0%BB%D1%8C%D1%88%D0%B8'
 			}
 		]
 	},
@@ -104,33 +99,19 @@ const history = [
 		title: 'Nie byli na Księżycu',
 		img: ['/cards/history/front_moon_pl.svg', '/cards/history/front_moon_by.svg'],
 		id: 4,
-		description: ['', '', ''],
+		description: [
+			'Białoruś i Polska nie mają programów kosmicznych mających na celu wylądowanie na Księżycu.'
+		],
 		sourcesPl: [
 			{
-				title: 'Wikipedia: ',
-				link: ''
-			},
-			{
-				title: 'Wikipedia: ',
-				link: ''
-			},
-			{
-				title: 'Wikipedia: ',
-				link: ''
+				title: 'Wikipedia: Strony w kategorii „Ludzie, którzy byli na Księżycu”',
+				link: 'https://pl.wikipedia.org/wiki/Kategoria:Ludzie,_kt%C3%B3rzy_byli_na_Ksi%C4%99%C5%BCycu'
 			}
 		],
 		sourcesBe: [
 			{
-				title: 'Wikipedia: ',
-				link: ''
-			},
-			{
-				title: 'Wikipedia: ',
-				link: ''
-			},
-			{
-				title: 'Wikipedia: ',
-				link: ''
+				title: 'Wikipedia: Список людей, побывавших на Луне',
+				link: 'https://ru.wikipedia.org/wiki/%D0%A1%D0%BF%D0%B8%D1%81%D0%BE%D0%BA_%D0%BB%D1%8E%D0%B4%D0%B5%D0%B9,_%D0%BF%D0%BE%D0%B1%D1%8B%D0%B2%D0%B0%D0%B2%D1%88%D0%B8%D1%85_%D0%BD%D0%B0_%D0%9B%D1%83%D0%BD%D0%B5'
 			}
 		]
 	},
@@ -138,33 +119,28 @@ const history = [
 		title: 'Duże protesty',
 		img: ['/cards/history/front_protest_pl.svg', '/cards/history/front_protest_by.svg'],
 		id: 5,
-		description: ['', '', ''],
+		description: [
+			'Państwo Polskie  ma bogatą historię strajków. Czasy trzech zaborów i realnego socjalizmu podtrzymywały w Polakach ducha buntu i poczucia powinności związanych z walką o swoje prawa. ',
+			'W historii Białorusi było też wiele protestów, głównie po upadku Związku Radzieckiego. Głównym celem protestów była dymisja Łukaszenki, ale były też protesty przeciwko przyjmowaniu różnych ustaw, poprawy jakości życia i protesty o prawa pracownicze.'
+		],
 		sourcesPl: [
 			{
-				title: 'Wikipedia: ',
-				link: ''
+				title: 'Wikipedia: Kategoria:Strajki w historii Polski',
+				link: 'https://pl.wikipedia.org/wiki/Kategoria:Strajki_w_historii_Polski'
 			},
 			{
-				title: 'Wikipedia: ',
-				link: ''
+				title: 'Najważniejsze strajki w historii Polski',
+				link: 'https://superbiz.se.pl/wiadomosci/najwazniejsze-strajki-w-historii-polski-galeria-aa-3LSf-FZSe-htPu.html'
 			},
 			{
-				title: 'Wikipedia: ',
-				link: ''
+				title: 'Wikipedia: Protesty na Białorusi (2020–2021)',
+				link: 'https://pl.wikipedia.org/wiki/Protesty_na_Bia%C5%82orusi_(2020%E2%80%932021)'
 			}
 		],
 		sourcesBe: [
 			{
-				title: 'Wikipedia: ',
-				link: ''
-			},
-			{
-				title: 'Wikipedia: ',
-				link: ''
-			},
-			{
-				title: 'Wikipedia: ',
-				link: ''
+				title: 'Wikipedia: Протесты в Белоруссии (2020—2021)',
+				link: 'https://ru.wikipedia.org/wiki/%D0%9F%D1%80%D0%BE%D1%82%D0%B5%D1%81%D1%82%D1%8B_%D0%B2_%D0%91%D0%B5%D0%BB%D0%BE%D1%80%D1%83%D1%81%D1%81%D0%B8%D0%B8_(2020%E2%80%942021)'
 			}
 		]
 	},
@@ -172,33 +148,24 @@ const history = [
 		title: 'Nie byli mistrzami piłki nożnej',
 		img: ['/cards/history/front_pilka_pl.svg', '/cards/history/front_pilka_by.svg'],
 		id: 6,
-		description: ['', '', ''],
+		description: [
+			'W historii mundiali Białoruś i Polska nigdy nie zdobyły Pucharu Świata.',
+			'Ale wielu zawodników Polskiego i Białoruskiego pochodzenia zyskało sławę i grało dla najlepszych klubów na świecie.'
+		],
 		sourcesPl: [
 			{
-				title: 'Wikipedia: ',
-				link: ''
+				title: 'Wikipedia: Mistrzostwa świata w piłce nożnej mężczyzn',
+				link: 'https://pl.wikipedia.org/wiki/Mistrzostwa_%C5%9Bwiata_w_pi%C5%82ce_no%C5%BCnej_m%C4%99%C5%BCczyzn'
 			},
 			{
-				title: 'Wikipedia: ',
-				link: ''
-			},
-			{
-				title: 'Wikipedia: ',
-				link: ''
+				title: 'Wikipedia: Tabela wszech czasów mistrzostw świata w piłce nożnej',
+				link: 'https://pl.wikipedia.org/wiki/Tabela_wszech_czas%C3%B3w_mistrzostw_%C5%9Bwiata_w_pi%C5%82ce_no%C5%BCnej'
 			}
 		],
 		sourcesBe: [
 			{
-				title: 'Wikipedia: ',
-				link: ''
-			},
-			{
-				title: 'Wikipedia: ',
-				link: ''
-			},
-			{
-				title: 'Wikipedia: ',
-				link: ''
+				title: 'Wikipedia: Список финалов чемпионатов мира по футболу',
+				link: 'https://ru.wikipedia.org/wiki/%D0%A1%D0%BF%D0%B8%D1%81%D0%BE%D0%BA_%D1%84%D0%B8%D0%BD%D0%B0%D0%BB%D0%BE%D0%B2_%D1%87%D0%B5%D0%BC%D0%BF%D0%B8%D0%BE%D0%BD%D0%B0%D1%82%D0%BE%D0%B2_%D0%BC%D0%B8%D1%80%D0%B0_%D0%BF%D0%BE_%D1%84%D1%83%D1%82%D0%B1%D0%BE%D0%BB%D1%83'
 			}
 		]
 	},
@@ -206,33 +173,20 @@ const history = [
 		title: 'Ogólna granica 398,624 km',
 		img: ['/cards/history/front_border_pl.svg', '/cards/history/front_border_by.svg'],
 		id: 7,
-		description: ['', '', ''],
+		description: [
+			'Granica polsko-białoruska – granica między Republiką Białorusi a Rzeczpospolitą Polską istniejąca formalnie od momentu uzyskania niepodległości Białorusi od Związku Radzieckiego, tj. od 25 sierpnia 1991 roku. Do 1991 roku obecna granica z Białorusią stanowiła część granicy z ZSRR i miała identyczny przebieg. Przez granicę prowadzi 13 przejść granicznych: 7 drogowych, 5 kolejowych i jedno rzeczne.',
+			'Granica polsko-białoruska jest zarazem zewnętrzną granicą Unii Europejskiej.'
+		],
 		sourcesPl: [
 			{
-				title: 'Wikipedia: ',
-				link: ''
-			},
-			{
-				title: 'Wikipedia: ',
-				link: ''
-			},
-			{
-				title: 'Wikipedia: ',
-				link: ''
+				title: 'Wikipedia: Granica polsko-białoruska',
+				link: 'https://pl.wikipedia.org/wiki/Granica_polsko-bia%C5%82oruska'
 			}
 		],
 		sourcesBe: [
 			{
-				title: 'Wikipedia: ',
-				link: ''
-			},
-			{
-				title: 'Wikipedia: ',
-				link: ''
-			},
-			{
-				title: 'Wikipedia: ',
-				link: ''
+				title: 'Wikipedia: Белорусско-польская граница',
+				link: 'https://ru.wikipedia.org/wiki/%D0%91%D0%B5%D0%BB%D0%BE%D1%80%D1%83%D1%81%D1%81%D0%BA%D0%BE-%D0%BF%D0%BE%D0%BB%D1%8C%D1%81%D0%BA%D0%B0%D1%8F_%D0%B3%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0'
 			}
 		]
 	},
@@ -240,33 +194,29 @@ const history = [
 		title: 'Podobne flagi',
 		img: ['/cards/history/front_flag_pl.svg', '/cards/history/front_flag_by.svg'],
 		id: 8,
-		description: ['', '', ''],
+		description: [
+			'Biało-czerwono-biała flaga Białorusi (biał. Бел-чырвона-белы сцяг, łacinka Bieł-čyrvona-bieły ściah, transkrypcja Bieł-czyrwona-bieły ściah) – historyczna flaga Białorusi przyjęta w 1918 roku przez władze Białoruskiej Republiki Ludowej, będąca również jej symbolem na emigracji, w latach 1991–1995 flaga państwowa Republiki Białorusi, obecnie używana jest jako symbol białoruskiej państwowości przez antyłukaszenkowską opozycję. Składa się z trzech równej wielkości pasów: dwóch białych i czerwonego między nimi. ',
+			'Według ustawy z dnia 31 stycznia 1980 r. o godle, barwach i hymnie Rzeczypospolitej Polskiej oraz o pieczęciach państwowych[9], jest nią prostokątny płat tkaniny o barwach Rzeczypospolitej Polskiej i proporcji 5:8, umieszczony na maszcie. Zgodnie z art. 6 ust. 2 ustawy, za flagę Polski uważany jest także wariant z godłem Polski, umieszczonym pośrodku białego pasa. ',
+			'Flagi Białorusi i Polski są bardzo podobne, zarówno pod względem kolorystyki, jak i układu elementów.'
+		],
 		sourcesPl: [
 			{
-				title: 'Wikipedia: ',
-				link: ''
+				title: 'Wikipedia: Biało-czerwono-biała flaga Białorusi',
+				link: 'https://pl.wikipedia.org/wiki/Bia%C5%82o-czerwono-bia%C5%82a_flaga_Bia%C5%82orusi'
 			},
 			{
-				title: 'Wikipedia: ',
-				link: ''
-			},
-			{
-				title: 'Wikipedia: ',
-				link: ''
+				title: 'Wikipedia: Flaga Polski',
+				link: 'https://pl.wikipedia.org/wiki/Flaga_Polski'
 			}
 		],
 		sourcesBe: [
 			{
-				title: 'Wikipedia: ',
-				link: ''
+				title: 'Wikipedia: Бело-красно-белый флаг',
+				link: 'https://ru.wikipedia.org/wiki/%D0%91%D0%B5%D0%BB%D0%BE-%D0%BA%D1%80%D0%B0%D1%81%D0%BD%D0%BE-%D0%B1%D0%B5%D0%BB%D1%8B%D0%B9_%D1%84%D0%BB%D0%B0%D0%B3'
 			},
 			{
-				title: 'Wikipedia: ',
-				link: ''
-			},
-			{
-				title: 'Wikipedia: ',
-				link: ''
+				title: 'Wikipedia: Флаг Польши',
+				link: 'https://ru.wikipedia.org/wiki/%D0%A4%D0%BB%D0%B0%D0%B3_%D0%9F%D0%BE%D0%BB%D1%8C%D1%88%D0%B8'
 			}
 		]
 	},
@@ -274,33 +224,33 @@ const history = [
 		title: 'Pożegnali się z USSR',
 		img: ['/cards/history/front_ussr_pl.svg', '/cards/history/front_ussr_by.svg'],
 		id: 9,
-		description: ['', '', ''],
+		description: [
+			'Rozpad ZSRR – proces rozpadu Związku Socjalistycznych Republik Radzieckich (ówcześnie największego państwa świata) zapoczątkowany ogłoszeniem suwerenności przez Estońską SRR 16 listopada 1988 i zakończony deklaracją o samorozwiązaniu ZSRR uchwaloną przez Radę Najwyższą ZSRR 26 grudnia 1991[1][2]. W okresie tym wszystkie piętnaście republik ZSRR ogłosiło suwerenność, wystąpiło z ZSRR i stało się niepodległymi państwami.',
+			'8 grudnia 1991 roku przywódcy Ukrainy i Białorusi, Leonid Krawczuk i Stanisław Szuszkiewicz, wspólnie z Jelcynem podpisali w Wiskulach na Białorusi porozumienia, które w miejsce ZSRR powoływały Wspólnotę Niepodległych Państw. Pozostałe republiki zaproszono do nowej federacji. Do końca 1991 roku Związek Radziecki, który powstał 30 grudnia 1922 roku, przestał istnieć na mocy prawa.'
+		],
 		sourcesPl: [
 			{
-				title: 'Wikipedia: ',
-				link: ''
+				title: 'Wikipedia: Rozpad ZSRR',
+				link: 'https://pl.wikipedia.org/wiki/Rozpad_ZSRR'
 			},
 			{
-				title: 'Wikipedia: ',
-				link: ''
+				title:
+					'„ZSRR jako zjawisko geopolityczne kończy swoje istnienie”. Jak wyglądały kulisy rozpadu imperium?',
+				link: 'https://www.newsweek.pl/swiat/polityka/rozpad-zsrr-uklad-bialowieski-jak-wygladaly-kulisy-upadku-zwiazku-radzieckiego/7l7wjne'
 			},
 			{
-				title: 'Wikipedia: ',
-				link: ''
+				title: 'Rozpad ZSRR',
+				link: 'https://dzieje.pl/infografiki/rozpad-zsrr'
 			}
 		],
 		sourcesBe: [
 			{
-				title: 'Wikipedia: ',
-				link: ''
+				title: 'Wikipedia: Белорусская Советская Социалистическая Республика',
+				link: 'https://ru.wikipedia.org/wiki/%D0%91%D0%B5%D0%BB%D0%BE%D1%80%D1%83%D1%81%D1%81%D0%BA%D0%B0%D1%8F_%D0%A1%D0%BE%D0%B2%D0%B5%D1%82%D1%81%D0%BA%D0%B0%D1%8F_%D0%A1%D0%BE%D1%86%D0%B8%D0%B0%D0%BB%D0%B8%D1%81%D1%82%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B0%D1%8F_%D0%A0%D0%B5%D1%81%D0%BF%D1%83%D0%B1%D0%BB%D0%B8%D0%BA%D0%B0'
 			},
 			{
-				title: 'Wikipedia: ',
-				link: ''
-			},
-			{
-				title: 'Wikipedia: ',
-				link: ''
+				title: 'Wikipedia: Распад СССР',
+				link: 'https://ru.wikipedia.org/wiki/%D0%A0%D0%B0%D1%81%D0%BF%D0%B0%D0%B4_%D0%A1%D0%A1%D0%A1%D0%A0'
 			}
 		]
 	}
