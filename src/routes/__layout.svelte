22909411937a553e0a5bd6423510b0bca39f0fad
DarkDev <darkdev_brand@riseup.net>
2023-05-03 12:25:29 +0300

    alfa v

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,151 +1,132 @@
 <script>
-    import '../app.css';  
+	import '../app.css';
 </script>
 
+<svelte:head>
+	<title>PL❤️BY</title>
+</svelte:head>
 
 <slot />
 
 <footer class="bg-red-600">
-  <div class=" py-8 px-6 flex flex-row justify-center">
-    <div class="ml-6 mr-6">
-      <h2 class="mb-6 text-sm font-semibold text-gray-200 uppercase ">O projekcie
-      </h2> 
-      <ul class="text-white">
-        <li class="mb-4">
-          <a href="https://by-pl.vercel.app/" class="hover:underline">GitHub repozytorium
-          </a>
-        </li>
-        
-        <li class="mb-4">
-          <a href="https://github.com/oleg-darkdev/by-PL/tree/master/static/cards" class="hover:underline">Pliki do pobrania
-          </a>
-        </li>
-        
-        <li class="mb-4">
-          <a href="https://github.com/oleg-darkdev/by-PL/tree/master/static/print" class="hover:underline">Pliki do wydrukowania
-          </a>
-        </li>
+	<div class=" py-8 px-6 flex flex-row justify-center">
+		<div class="ml-6 mr-6">
+			<h2 class="mb-6 text-sm font-semibold text-gray-200 uppercase ">O projekcie</h2>
+			<ul class="text-white">
+				<li class="mb-4">
+					<a href="https://github.com/oleg-darkdev/by-PL" class="hover:underline"
+						>GitHub repozytorium
+					</a>
+				</li>
 
-      </ul>
-      
-    </div> 
+				<li class="mb-4">
+					<a
+						href="https://github.com/oleg-darkdev/by-PL/tree/master/static/cards"
+						class="hover:underline"
+						>Pliki do pobrania
+					</a>
+				</li>
 
-    <div class="ml-6 mr-6">
-      <h2 class="mb-6 text-sm font-semibold text-gray-200 uppercase ">Strona główna
-      </h2> 
-      <ul class="text-white">
-        <li class="mb-4">
-          <a href="/#history" class="hover:underline">Historia
-          </a>
-        </li>
-        
-        <li class="mb-4">
-          <a href="/#culture" class="hover:underline">Kultura
-          </a>
-        </li>
-        
-        <li class="mb-4">
-          <a href="/#territory" class="hover:underline">Teren
-          </a>
-        </li>
-        
-        <li class="mb-4">
-          <a href="/#feast" class="hover:underline">Święta
-          </a>
-        </li>
-      </ul>
-      
-    </div> 
-    <div class="ml-6 mr-6">
-      <h2 class="mb-6 text-sm font-semibold uppercase text-gray-200 ">Web-Aplikacja 
-      </h2> 
-      <ul class="text-white">
-        <li class="mb-4">
-          <a href="/app#history" class="hover:underline">Historia
-          </a>
-        </li>
-        
-        <li class="mb-4">
-          <a href="/app#culture" class="hover:underline">Kultura
-          </a>
-        </li>
-        
-        <li class="mb-4">
-          <a href="/app#territory" class="hover:underline">Teren
-          </a>
-        </li>
-        
-        <li class="mb-4">
-          <a href="/app#feast" class="hover:underline">Święta
-          </a>
-        </li>
-        
-      </ul>
-      
-    </div> 
-	<div class="ml-6 mr-6">
-      <h2 class="mb-6 text-sm font-semibold uppercase text-gray-200 ">Skontaktuj się z autorem
-      </h2> 
-      <ul class="text-white">
-        <li class="mb-4">
-          <a href="mailto:oleg@darkdev.games" class="hover:underline">oleg@darkdev.games
-          </a>
-        </li>
-        
-        <li class="mb-4">
-          <a href="https://github.com/oleg-darkdev" class="hover:underline">GitHub
-          </a>
-        </li>
-		<li class="mb-4">
-          <a href="https://www.linkedin.com/in/oleg-darkdev?original_referer=https%3A%2F%2Flinktr.ee%2F" class="hover:underline">Linkedin
-          </a>
-        </li>
-		<li class="mb-4">
-          <a href="https://www.facebook.com/hulio.mondre" class="hover:underline">Facebook
-          </a>
-        </li>
-        
-        <li class="mb-4">
-          <a href="https://linktr.ee/darkdev" class="hover:underline">Linktr.ee
-          </a>
-        </li>
-        
+				<li class="mb-4">
+					<a
+						href="https://github.com/oleg-darkdev/by-PL/tree/master/static/print"
+						class="hover:underline"
+						>Pliki do wydrukowania
+					</a>
+				</li>
+			</ul>
+		</div>
 
-        
-      </ul>
-      
-    </div>
-  </div>
+		<div class="ml-6 mr-6">
+			<h2 class="mb-6 text-sm font-semibold text-gray-200 uppercase ">Strona główna</h2>
+			<ul class="text-white">
+				<li class="mb-4">
+					<a href="/#history" class="hover:underline">Historia </a>
+				</li>
+
+				<li class="mb-4">
+					<a href="/#culture" class="hover:underline">Kultura </a>
+				</li>
+
+				<li class="mb-4">
+					<a href="/#territory" class="hover:underline">Teren </a>
+				</li>
+
+				<li class="mb-4">
+					<a href="/#feast" class="hover:underline">Święta </a>
+				</li>
+			</ul>
+		</div>
+		<div class="ml-6 mr-6">
+			<h2 class="mb-6 text-sm font-semibold uppercase text-gray-200 ">Web-Aplikacja</h2>
+			<ul class="text-white">
+				<li class="mb-4">
+					<a href="/app#history" class="hover:underline">Historia </a>
+				</li>
+
+				<li class="mb-4">
+					<a href="/app#culture" class="hover:underline">Kultura </a>
+				</li>
+
+				<li class="mb-4">
+					<a href="/app#territory" class="hover:underline">Teren </a>
+				</li>
+
+				<li class="mb-4">
+					<a href="/app#feast" class="hover:underline">Święta </a>
+				</li>
+			</ul>
+		</div>
+		<div class="ml-6 mr-6">
+			<h2 class="mb-6 text-sm font-semibold uppercase text-gray-200 ">Skontaktuj się z autorem</h2>
+			<ul class="text-white">
+				<li class="mb-4">
+					<a href="https://github.com/oleg-darkdev" class="hover:underline">GitHub </a>
+				</li>
+				<li class="mb-4">
+					<a
+						href="https://www.linkedin.com/in/oleg-darkdev?original_referer=https%3A%2F%2Flinktr.ee%2F"
+						class="hover:underline"
+						>Linkedin
+					</a>
+				</li>
+				<li class="mb-4">
+					<a href="https://www.facebook.com/hulio.mondre" class="hover:underline">Facebook </a>
+				</li>
+
+				<li class="mb-4">
+					<a href="https://linktr.ee/darkdev" class="hover:underline">Linktr.ee </a>
+				</li>
+			</ul>
+		</div>
+	</div>
 </footer>
 
-
 <style>
-    
-  :global(.font-impact) {
-          font-family: 'Impact';
-            line-height: 1.2;
-  }
+	:global(.font-impact) {
+		font-family: 'Impact';
+		line-height: 1.2;
+	}
 
-    @font-face {
-        font-family: 'Impact';
-        font-style: normal;
-        font-weight: 500;
-        
-        src: local(''),
-            /* Super Modern Browsers */ url('/fonts/impact.woff') format('woff'),
-            /* Modern Browsers */ url('/fonts/impact.ttf') format('truetype'),
-            /* Safari, Android, iOS */ url('/fonts/impact.svg') format('svg'); /* Legacy iOS */
-    }
+	@font-face {
+		font-family: 'Impact';
+		font-style: normal;
+		font-weight: 500;
 
-	:global(.justify-center, section)  {
-        display: flex;
-        justify-content: center;
-    }
-  
-    :global(.bg) {
-        background-repeat: no-repeat;
-        background-position: center;
-    }
+		src: local(''), /* Super Modern Browsers */ url('/fonts/impact.woff') format('woff'),
+			/* Modern Browsers */ url('/fonts/impact.ttf') format('truetype'),
+			/* Safari, Android, iOS */ url('/fonts/impact.svg') format('svg'); /* Legacy iOS */
+	}
+
+	:global(.justify-center, section) {
+		display: flex;
+		justify-content: center;
+	}
+
+	:global(.bg) {
+		background-repeat: no-repeat;
+		background-position: center;
+	}
 
 	:global(body, html) {
 		height: 100%;
@@ -153,7 +134,7 @@
 		padding: 0;
 	}
 
-    @media (min-width: 1024px) {
+	@media (min-width: 1024px) {
 		:global(.bg-image-wrap) {
 			min-height: 100vh;
 		}
@@ -170,7 +151,6 @@
 		}
 		:global(.feast) {
 			background-image: url(/images/bg/feast.svg);
-		}		
+		}
 	}
-
 </style>
--- src/routes/app.svelte
+++ src/routes/app.svelte
@@ -1,101 +1,109 @@
-<svelte:head>
-	<title>PL❤️BY</title>
-</svelte:head>
-
 <script>
-    import cardCategories from '../data/cardCategories';
-    import { Breadcrumb, BreadcrumbItem } from 'flowbite-svelte';
+	import cardCategories from '../data/cardCategories';
+	import { Breadcrumb, BreadcrumbItem } from 'flowbite-svelte';
 </script>
 
 <main class="pt-20 p-2 flex flex-col items-center">
-    <div class="p-12 bg-gray-200 max-w-2xl rounded-lg justify-center flex-col">
-        <Breadcrumb  aria-label="Solid background breadcrumb example" solid>
-            <BreadcrumbItem href="/" home>Strona główna</BreadcrumbItem>
-            <BreadcrumbItem href="/app">app</BreadcrumbItem>
-        </Breadcrumb>
+	<div class="p-12 bg-gray-200 max-w-2xl rounded-lg justify-center flex-col">
+		<Breadcrumb aria-label="Solid background breadcrumb example" solid>
+			<BreadcrumbItem href="/" home>Strona główna</BreadcrumbItem>
+			<BreadcrumbItem href="/app">app</BreadcrumbItem>
+		</Breadcrumb>
 
-        <h1 class="mt-4 text-5xl text-center font-impact tracking-tight text-gray-700">
-            Nawigacja
-        </h1>
-          <ul class="relative w-full text-gray-600 text-dark max-w-md list-disc list-inside space-y-1">
-                {#each cardCategories as category}
-                    <li data-aos="fade-up" class="w-full p-1 text-3xl font-semibold rounded-md  text-dark hover:bg-white">
-                        <a href='#{category.id}' class="inline-flex items-center text-gray-700 hover:underline">{category.title}
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 ml-2">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25">
-                                </path>
-                            </svg>
-                        </a>
-                    </li>
-                {/each}
-            </ul>
+		<h1 class="mt-4 text-5xl text-center font-impact tracking-tight text-gray-700">Nawigacja</h1>
+		<ul class="relative w-full text-gray-600 text-dark max-w-md list-disc list-inside space-y-1">
+			<li
+				data-aos="fade-up"
+				class="w-full p-1 text-3xl font-semibold rounded-md  text-dark hover:bg-white"
+			>
+				<a href="/manual" class="inline-flex items-center text-gray-700 hover:underline"
+					>Instrukcja
+				</a>
+			</li>
+			{#each cardCategories as category}
+				<li
+					data-aos="fade-up"
+					class="w-full p-1 text-3xl font-semibold rounded-md  text-dark hover:bg-white"
+				>
+					<a href="#{category.id}" class="inline-flex items-center text-gray-700 hover:underline"
+						>{category.nav}
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="1.5"
+							stroke="currentColor"
+							class="w-5 h-5 ml-2"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+							/>
+						</svg>
+					</a>
+				</li>
+			{/each}
+		</ul>
+	</div>
 
-    </div>
-        
-    {#each cardCategories as category}
-        <section class="w-full m-4 flex flex-row   justify-center ">
+	{#each cardCategories as category}
+		<section class="w-full m-4 flex flex-row   justify-center ">
+			<div class="bg-section lg:w-2/12 -ml-10 md:w-3/12 {category.miniImg}" />
 
-            <div class="bg-section lg:w-2/12 -ml-10 md:w-3/12 {category.miniImg}"></div>
-            <!-- <div class="flex flex-row w-full">
-            <div class="flex flex-col items-end mb-4 w-4/12">
-                <h2>{category.title}</h2>
-                <ul class="relative ml-10 mb-4 text-dark max-w-md list-disc list-inside space-y-1">
-                    {#each category.listData as data}
-                    <li data-aos="fade-up" class="font-semibold rounded-md  text-dark hover:bg-white">
-                        {data}
-                    </li>
-                    {/each}
-                </ul>
-            </div> -->
-            <a name={category.id}></a>
-            <div class="hover:bg-red-300 xl:p-12  lg:p-12 md:p-12 p-2 bg-gray-200 max-w-4xl rounded-lg justify-center flex-col">
-                
-                <h2 class="mb-2 text-8xl text-left font-impact tracking-tight text-gray-700">{category.title}</h2>
-                
-                <div class="flex flex-row justify-center flex-wrap">
-                    
-                    {#each category.cardsData as cardData}
-                        <a href="/app/card/{cardData.id}" class="bg-red-500 w-40 h-40 rounded-lg border border-gray-200 dark:border-gray-700 shadow-md flex  flex-col hover:bg-gray-800 p-4 sm:p-6" >
-                            <h5 class="mb-2 text-8xl text-center font-impact tracking-tight text-white">
-                                {cardData.id}
-                            </h5> 
-                            <!-- <p class="font-normal text-gray-700 dark:text-gray-400 leading-tight">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
-                            </p> -->
-                        </a>
-                    {/each}
-                </div>
-            </div>
-        </section>
-    {/each}
+			<a name={category.id} />
+			<div
+				class="hover:bg-red-300 xl:p-12  lg:p-12 md:p-12 p-2 bg-gray-200 max-w-4xl rounded-lg justify-center flex-col"
+			>
+				<h2 class="mb-2 text-8xl text-left font-impact tracking-tight text-gray-700">
+					{category.title}
+				</h2>
+
+				<div class="flex flex-row justify-center flex-wrap">
+					{#each category.cardsData as cardData}
+						<a
+							href="/app/card/{cardData.id}"
+							style="background-image: url({cardData.img[1]})"
+							class="bg-red-500 w-40 h-40 rounded-lg border bg-cover bg-center border-gray-200 dark:border-gray-700 shadow-md flex  flex-col hover:bg-gray-800 p-4 sm:p-6"
+						>
+							<h5 class="mb-2 text-8xl text-center font-impact tracking-tight text-gray-900">
+								{cardData.id}
+							</h5>
+						</a>
+					{/each}
+				</div>
+			</div>
+		</section>
+	{/each}
 </main>
 
-
 <style>
-    .bg-section {
-		/* align-content: center; */
-		/* display: flex; */
+	.bg-section {
 		height: auto;
-		background-repeat: no-repeat;		
+		background-repeat: no-repeat;
 		background-size: auto;
-        height: 300px;
-        background-position: right;
+		height: 300px;
+		background-position: right;
 	}
 
-		.history-bg-mini {
-			background-image: url(/images/bg/history_mini.svg);
-		}
-		.culture-bg-mini {
-			background-image: url(/images/bg/culture_mini.svg);
-		}
-		.territory-bg-mini {
-			background-image: url(/images/bg/territory_mini.svg);
-		}
+	.history-bg-mini {
+		background-image: url(/images/bg/history_mini.svg);
+	}
+	.culture-bg-mini {
+		background-image: url(/images/bg/culture_mini.svg);
+	}
+	.territory-bg-mini {
+		background-image: url(/images/bg/territory_mini.svg);
+	}
+	.feast-bg-mini {
+		background-image: url(/images/bg/feast_mini.svg);
+	}
+	@media (max-width: 760px) {
+		.history-bg-mini,
+		.culture-bg-mini,
+		.territory-bg-mini,
 		.feast-bg-mini {
-			background-image: url(/images/bg/feast_mini.svg);
+			display: none;
 		}
-    @media (max-width: 760px) {
-		.history-bg-mini, .culture-bg-mini, .territory-bg-mini, .feast-bg-mini {
-            display: none;
-        }
-	}	
+	}
 </style>
