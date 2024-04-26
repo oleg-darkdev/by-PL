698871d4b2b699a626210faf715b809be5dd393d
DarkDev <darkdev_brand@riseup.net>
2024-04-26 19:31:25 +0300

    add partners projects logos

---
+++ src/components/DarkDevContact.svelte
@@ -0,0 +1,80 @@
+<div class="flex flex-col justify-start">
+    <!-- <div class="py-1 max-w-[140px] ">
+		<h2 class="text-white text-center ">Autor i twórca <br /> Oleg Medvedev</h2>
+		<div class="flex flex-row flex-wrap justify-center">
+			<a target="_blank" class="" href="https://www.facebook.com/hulio.mondre">
+					<i style='' class="mr-3 text-3xl text-white fa-brands fa-facebook " />
+			</a>
+
+			<a target="_blank" href="https://www.linkedin.com/in/oleg-darkdev">
+				<i class="fa-brands mr-3 fa-linkedin-in text-4xl text-white" />
+			</a>
+			<a target="_blank" href="https://github.com/oleg-darkdev">
+				<i class="fa-brands fa-github text-3xl text-white" />
+			</a>
+		</div>
+	</div> -->
+    <div class="py-1 max-w-[140px] ">
+		<a href="https://darkdev-progect.vercel.app/" class="">
+			<h2 class="text-white text-center ">Projektowanie gry:</h2>
+			<img src="/logos/44games.svg" class="mx-auto h-16 w-auto" alt="" />
+		</a>
+	</div>
+	<div class="py-1 max-w-[200px] ">
+		<a href="https://darkdev-progect.vercel.app/" class="">
+			<h2 class="text-white text-start -mb-2">Strona stworzona przez:</h2>
+			<img src="/logos/darkdev.svg" class="mx-auto h-14 w-auto" alt="" />
+		</a>
+	</div>
+	
+	
+</div>
+<!-- 
+<div class="wrap-social-container mb-8 flex-col items-center justify-center">
+    <div class="email-animation-container">
+        <i class="icon-envelope email-icon"></i>
+    </div>
+    <h2 class="w-full text-center lg:text-3xl sm:text-2xl text-white font-black m-0"><i class="mr-2 icon-envelope text-lightYellow"></i>darkdev_brand@riseup.net</h2>
+
+</div> -->
+
+<!-- <a href="https://linktr.ee/darkdev" class="hover:underline">Linktr.ee </a> -->
+<style>
+	@-webkit-keyframes wiggle {
+		0% {
+			transform: rotateZ(2deg);
+		}
+		50% {
+			transform: rotateZ(-2deg);
+		}
+		100% {
+			transform: rotateZ(2deg);
+		}
+	}
+
+	.wrap-email-container {
+		width: 100%;
+		text-align: center;
+	}
+
+	.email-animation-container {
+		margin-top: -150px;
+		font-size: 12em;
+		height: 240px;
+		/* max-width: 360px; */
+		overflow: hidden;
+		border-bottom: 1px solid #ffc800;
+	}
+
+	.email-icon {
+		position: relative;
+		top: 55px;
+		margin: 0 10px;
+		transition: all 100ms cubic-bezier(0.42, 0, 0.58, 1);
+		color: #fff;
+		/* ease-in-out */
+	}
+	.email-icon:hover {
+		top: 5px;
+	}
+</style>
